c743a921ec200285ea45c9d60063b0770f993338
Ruslan Dovhanych <imdakris@gmail.com>
2024-01-31 08:13:29 +0200

    fix

--- main.py
+++ main.py
@@ -1,12 +1,68 @@
+"""Writing a card game using classes"""
+import random
+
+
+class Card:
+    """Description of playing card"""
+
+    def __init__(self, rank: str, suit: str) -> None:
+        self.rank = rank
+        self.suit = suit
+
+    def get_value(self) -> int:
+        """Returns playing card points"""
+        if self.rank in "AJQK":  # "Ace, Jack, Queen, King"
+            return 10
+        else:
+            return " A23456789".index(self.rank)
+
+    def get_rank(self) -> str:
+        """Displays playing card information"""
+        return f"{self.suit}{self.rank}"
+
+
+class DeskCard:
+    """Creates a deck of playing cards"""
+
+    def __init__(self) -> None:
+        _rank = "A23456789AJQK"
+        _suit = "CDHS"  # clubs (♣), diamonds (♦), hearts (♥), spades (♠)
+        self.__cards = [Card(r, s) for s in _suit for r in _rank]
+        random.shuffle(self.__cards)
+
+    def get_card(self) -> Card:
+        """To distribute playing cards"""
+        return self.__cards.pop()
+
+
+class Player:
+    """Creates and describes a player"""
+
+    def __init__(self, name: str) -> None:
+        self._hand = []
+        self.count = 0
+        self.name = name
+
+    @property
+    def hand(self) -> str:
+        """Informs the player about his cards and points"""
+        return f"Playing cards in hand: {self._hand}; Points you have:{self.count}"
+
+    @hand.setter
+    def hand(self, card: Card) -> None:
+        self.count += card.get_value()
+        self._hand.append(card.get_rank())
+
+
 class Dealer(Player):
     """Creates a dealer"""
 
-    def deal_cards(self, deck: DeskCard) -> None:
-        """Deal cards based on the dealer's rule"""
+    def get_cards(self, cards: DeskCard):
+        """Rule"""
         while self.count < 21:
-            card = deck.get_card()
-            if card.get_value() + self.count <= 21:
-                self.hand = card
+            _card = cards.get_card()
+            if _card.get_value() + self.count <= 21:
+                self.hand = _card
             else:
                 break
 
@@ -20,52 +76,45 @@ class Game:
         self.dealer = Dealer(name="Dealer")
 
     def start(self) -> None:
-        """Deal 2 cards to the player and display information about their cards"""
+        """We give 2 cards to the player and display information about his cards"""
         self.player.hand = self.cards.get_card()
         self.player.hand = self.cards.get_card()
         self.dealer.hand = self.cards.get_card()
         self.dealer.hand = self.cards.get_card()
         print(self.player.hand)
-
         while self.player.count < 21:
-            answer = input("Do you want another card? (yes/no): ")
-            if answer.lower() == "yes":
+            answer = input("Card? y/n: ")
+            if answer == "y":
                 self.player.hand = self.cards.get_card()
                 print(self.player.hand)
-            elif answer.lower() == "no":
-                self.dealer.deal_cards(self.cards)
+            elif answer == "n":
+                self.dealer.get_cards(self.cards)
                 break
-
         self.check_count()
 
-    def display_info(self) -> str:
-        """Display player and dealer information"""
+    def print(self) -> str:
+        """Print"""
         return f"\n{self.player.name}:\n{self.player.hand}\n{self.dealer.name}:\n{self.dealer.hand}"
 
     def check_count(self) -> None:
-        """Determine the winner of the game"""
+        """Determines the winner of the game"""
         if self.player.count > 21:
-            print("You lose", self.display_info())
+            print("You lose", self.print())
         elif self.dealer.count > 21 and self.player.count <= 21:
-            print("You won!!!", self.display_info())
+            print("You won!!!", self.print())
         elif self.dealer.count == self.player.count:
-            print("Draw...", self.display_info())
+            print("Draw...", self.print())
         elif self.dealer.count > self.player.count:
-            print("You lose", self.display_info())
+            print("You lose", self.print())
         elif self.dealer.count < self.player.count:
-            print("You won!!!", self.display_info())
+            print("You won!!!", self.print())
 
 
 def main() -> None:
-    """Start the game"""
-    while True:
-        name = input("Your name?: ")
-        game = Game(name)
-        game.start()
-
-        play_again = input("Do you want to play again? (yes/no): ")
-        if play_again.lower() != "yes":
-            break
+    """Starts the game"""
+    name = input("Your name?: ")
+    game = Game(name)
+    game.start()
 
 
 if __name__ == "__main__":
